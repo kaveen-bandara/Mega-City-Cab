2816cf0bc2ba5dd0019521d4799a269e183255af
Chaanya <kaveenbandara1018@gmail.com>
2025-03-14 10:11:18 +0530

    v1
    First version deployed

--- backend/src/main/java/com/megaCityCab/backend/repository/UserRepository.java
+++ backend/src/main/java/com/megaCityCab/backend/repository/UserRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
 
     boolean existsByEmail(String email);
-
-    Optional<User> findByEmail(String  email);
+     Optional<User> findByEmail(String  email);
 }
